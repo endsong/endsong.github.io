722dee2ed6222e9242170a3052f8e869d7125657
Alexander Henne <alexander@henne.nu>
2022-01-03 00:39:02 +0100

    Indent with 2 spaces, update license author

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,83 +1,83 @@
 <script>
-	import { liveQuery } from "dexie";
-	import { db } from "./db";
+  import { liveQuery } from "dexie";
+  import { db } from "./db";
 
-	let count = liveQuery(() => db.plays.count());
+  let count = liveQuery(() => db.plays.count());
 
-	let files;
-	let query;
-	let queryResult;
+  let files;
+  let query;
+  let queryResult;
 
-	async function getJSON(blob) {
-		const text = await blob.text();
-		return JSON.parse(text);
-	}
+  async function getJSON(blob) {
+    const text = await blob.text();
+    return JSON.parse(text);
+  }
 
-	async function loadFiles() {
-		for (const file of files) {
-			const plays = await getJSON(file);
-			for (let play of plays) {
-				if (play.ts) {
-					play.ts = play.ts.replace(" UTC", "Z");
-				}
-			}
-			await db.plays.bulkAdd(plays);
-		}
-	}
+  async function loadFiles() {
+    for (const file of files) {
+      const plays = await getJSON(file);
+      for (let play of plays) {
+        if (play.ts) {
+          play.ts = play.ts.replace(" UTC", "Z");
+        }
+      }
+      await db.plays.bulkAdd(plays);
+    }
+  }
 
-	async function runQuery() {
-		try {
-			return eval(query);
-		} catch (error) {
-			return error;
-		}
-	}
+  async function runQuery() {
+    try {
+      return eval(query);
+    } catch (error) {
+      return error;
+    }
+  }
 </script>
 
 <div class="mx-auto max-w-lg mt-4 bg-gray-100 dark:bg-gray-800 rounded-lg">
-	<div class="align-middle mx-auto">
-		<input type="file" accept=".json" multiple bind:files>
+  <div class="align-middle mx-auto">
+    <input type="file" accept=".json" multiple bind:files>
 
-		{#if files && files[0]}
-		{#await loadFiles()}
-			<p>
-				Loading files..
-			</p>
-		{:then text}
-			<p>
-				{text}
-			</p>
-		{:catch error}
-			<p style="color: red">{error.message}</p>
-		{/await}
-		{/if}
+    {#if files && files[0]}
+    {#await loadFiles()}
+      <p>
+        Loading files..
+      </p>
+    {:then text}
+      <p>
+        {text}
+      </p>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+    {/if}
 
-		{$count == null ? "" : $count + " plays loaded"}
-	</div>
+    {$count == null ? "" : $count + " plays loaded"}
+  </div>
 
-	<textarea bind:value={query} on:input={() => queryResult = runQuery()}></textarea>
+  <textarea bind:value={query} on:input={() => queryResult = runQuery()}></textarea>
 
-	{#if query}
-	{#await queryResult}
-		<p>
-			Evaluating..
-		</p>
-	{:then text}
-		<p>
-			{text}
-		</p>
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
-	{/if}
+  {#if query}
+  {#await queryResult}
+    <p>
+      Evaluating..
+    </p>
+  {:then text}
+    <p>
+      {text}
+    </p>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+  {/if}
 </div>
 
 <style global lang="postcss">
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 
-	:global(body) {
-		@apply bg-blue-500;
-	}
+  :global(body) {
+    @apply bg-blue-500;
+  }
 </style>
